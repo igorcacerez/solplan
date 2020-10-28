e9185795b0b362da6a1d7c50f65f48aaaa1b7cdf
Igor Cacerez <igor.cacerez@gmail.com>
2020-10-28 17:25:23 -0300

    include

---
+++ include/footer.php
@@ -0,0 +1,84 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="assets/img/logo.png" class="logo" />
+
+            </div>
+
+            <div class="col-md-6">
+                <p>
+                    Av. Brasília, 2121, 8º and. - Vila São Paulo <br>
+                    CEP: 16015-460 - Araçatuba - SP
+                </p>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding-top: 33px;">
+                    <a href="https://www.fb.com/solplan.oficial/" target="_blank" class="menu-itens-mobile" style="padding: 0px 6px; vertical-align: super">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+
+                    <a href="https://www.instagram.com/solplan.oficial/" target="_blank" class="menu-itens-mobile" style="padding: 0px 3px; vertical-align: super">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+
+                    <a href="https://api.whatsapp.com/send?phone=5518997814900" target="_blank" class="menu-itens-mobile" style="padding: 0px 6px; vertical-align: super">
+                        <i class="fab fa-whatsapp"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-2987821-92"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'UA-2987821-92');
+</script>
+
+
+<script type="text/javascript" src="assets/_vendors/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="assets/_vendors/bootstrap/js/popper.min.js"></script>
+<script type="text/javascript" src="assets/_vendors/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/_vendors/owlcarousel/owl.carousel.min.js"></script>
+
+<script type="text/javascript" src="assets/_vendors/jquery/jquery.mask.min.js"></script>
+<script type="text/javascript" src="assets/_vendors/jquery/jquery.maskMoney.js"></script>
+<script type="text/javascript" src="assets/_vendors/fancybox3/jquery.fancybox.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<script type="text/javascript" src="assets/js/appa6f5.js?v=57"></script>
+
+<script src="assets/js/simuladora6f5.js?v=57" type="text/javascript"></script>
+<script src="assets/js/instaFeed.min.js"></script>
+<script>
+    (function() {
+
+        new InstagramFeed({
+            'username': 'solplan.oficial',
+            'container': document.getElementById("instagram-feed2"),
+            'display_profile': false,
+            'display_biography': false,
+            'display_gallery': true,
+            'callback': null,
+            'styling': true,
+            'items': 6,
+            'items_per_row': 6,
+            'margin': 0
+        });
+
+    })();
+</script>
+</body>
+</html>
--- include/header.php
+++ include/header.php
@@ -1,1 +1,230 @@
-<?php
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="author" content="SOLPLAN | Consultoria Energética">
+    <meta content="Nós vamos além da energia. Temos as melhores soluções para você, sua empresa e sua cidade."
+          name="description">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+    <meta name="theme-color" content="#2083B6">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#2083B6">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="robots" content="index,follow">
+    <meta name="googlebot-news" content="index,follow">
+    <meta name="keywords"
+          content="energia solar, plataforma solar, dimensionamento energia solar, integrador solar, parceiro solar, distribuidor solar">
+    <meta name="msapplication-TileImage" content="assets/img/icons/icons/icon-144x144.html">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/icons/icons/icon-72x72.html">
+    <link rel="apple-touch-icon" sizes="96x96" href="assets/img/icons/icons/icon-96x96.html">
+    <link rel="apple-touch-icon" sizes="128x128" href="assets/img/icons/icons/icon-128x128.html">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/icons/icons/icon-144x144.html">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/icons/icons/icon-152x152.html">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/icons/icons/icon-192x192.html">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/icons/icons/icon-96x96.html">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/icons/icons/Icon-16.html">
+    <link rel="shortcut icon" href="assets/img/icons/icons/Icon-16.html" type="image/png">
+
+    <meta property="og:title" content="SOLPLAN | Consultoria Energética">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="pt_BR"/>
+    <meta property="og:url" content="index.html">
+    <meta property="og:image" content="assets/img/icons/fb_marca.html">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta property="og:image:with" content="600">
+    <meta property="og:image:height" content="480">
+    <meta property="og:description"
+          content="Nós vamos além da energia. Temos as melhores soluções para você, sua empresa e sua cidade.">
+
+    <!-- Required meta tags -->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/png">
+
+    <link rel="stylesheet" href="assets/_vendors/bootstrap/css/bootstrap.min.css">
+    <link href="assets/_vendors/owlcarousel/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/_vendors/owlcarousel/owl.theme.default.min.css" rel="stylesheet">
+    <link href="assets/_vendors/fancybox3/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="assets/css/theme_1581994101101.css" rel="stylesheet">
+    <link href="assets/css/appa6f5.css?v=57" rel="stylesheet">
+    <link href="assets/css/estilo.css" rel="stylesheet">
+    <link href="assets/css/responsivo.css" rel="stylesheet">
+
+    <title>SOLPLAN | Consultoria Energética</title>
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            '../connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '232311154858894');
+        fbq('track', 'PageView');
+    </script>
+
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=232311154858894&amp;ev=PageView&amp;noscript=1"
+        /></noscript>
+    <!-- End Facebook Pixel Code -->
+
+
+    <!-- FontIcone -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+          integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+          crossorigin="anonymous">
+</head>
+
+<body>
+
+
+<!-- RODAPE FIXO -->
+
+<div class="rodape-fixo">
+    <img src="assets/img/contato.png" class="img-div" />
+    <img src="assets/img/endereco.png" class="img-div" />
+    <a href="https://api.whatsapp.com/send?phone=5518997814900" target="_blank" class="abre-link">
+        <img src="assets/img/whats.png" />
+    </a>
+</div>
+
+<!-- --------------->
+
+
+
+
+<!-- MOBILE -->
+<div class="sidebar-menu-mobile">
+    <div class="row">
+        <div style="height: 95px;width: 95%;background-color: #794fb1;text-align: center">
+            <a href="index.php">
+                <img style="width: 150px;margin-top: 12px;" src="assets/img/logo-branca.png">
+            </a>
+        </div>
+    </div>
+
+
+
+    <div class="container">
+        <div style="margin-top: 30px" class="row">
+            <div class="col-md-12">
+                <a href="index.php" class="menu-itens-mobile">HOME</a>
+
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <a href="quem-somos.php" class="menu-itens-mobile">QUEM SOMOS </a>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <a href="index.php#solucoes" class="menu-itens-mobile">SOLUÇÕES</a>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <a href="como-funciona.php" class="menu-itens-mobile">COMO FUNCIONA</a>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <a href="index.php#clientes" class="menu-itens-mobile">CLIENTES</a>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row text-left">
+            <div style="padding: 0px 30px;" class="col-md-12">
+
+                <a href="https://www.fb.com/solplan.oficial/" target="_blank" class="menu-itens-mobile" style="padding: 0px 3px; vertical-align: super">
+                    <i class="fab fa-facebook"></i>
+                </a>
+
+                <a href="https://www.instagram.com/solplan.oficial/" target="_blank" class="menu-itens-mobile" style="padding: 0px 3px; vertical-align: super">
+                    <i class="fab fa-instagram"></i>
+                </a>
+
+                <a href="https://api.whatsapp.com/send?phone=5518997814900" target="_blank" class="menu-itens-mobile" style="padding: 0px 3px; vertical-align: super">
+                    <i class="fab fa-whatsapp"></i>
+                </a>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<div class="fundo-menu" onclick="menu('fecha')"></div>
+
+<!-- END ---->
+
+
+
+<header class="header-menu">
+    <div class="container">
+        <div class="row" style="position:relative;">
+
+            <div class="col-md-3">
+                <img src="assets/img/logo-branca.png" style="width: 145px;" alt="SOLPLAN | Consultoria Energética"
+                     class="img-fluid ">
+            </div>
+
+            <div class="col-md-6 text-center some">
+                <ul>
+                    <li> <a href="index.php"> Home </a> </li>
+                    <li> <a href="quem-somos.php"> Quem Somos </a> </li>
+                    <li> <a href="index.php#solucoes"> Soluções </a> </li>
+                    <li> <a href="como-funciona.php"> Como funciona </a> </li>
+                    <li> <a href="index.php#clientes"> Clientes </a> </li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 text-center some">
+                <a href="https://www.fb.com/solplan.oficial/" class="icone-social">
+                    <i class="fab fa-facebook"></i>
+                </a>
+
+                <a href="https://www.instagram.com/solplan.oficial/" class="icone-social">
+                    <i class="fab fa-instagram"></i>
+                </a>
+
+                <a href="https://api.whatsapp.com/send?phone=5518997814900" class="icone-social">
+                    <i class="fab fa-whatsapp"></i>
+                </a>
+            </div>
+
+            <button class="abreMenu" onclick="menu('abre')">
+                <i class="fas fa-bars"></i>
+            </button>
+
+        </div>
+    </div>
+</header>
+
