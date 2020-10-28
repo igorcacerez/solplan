06e4f87ad703c2452318989158311846e72a5351
Igor Cacerez <igor.cacerez@gmail.com>
2020-10-28 18:04:18 -0300

    Subnindo

---
+++ como-funciona.php
@@ -0,0 +1,72 @@
+<?php include ("include/header.php"); ?>
+
+<style>
+    footer
+    {
+        background: #f1f2f4 !important;
+    }
+</style>
+
+<!-- //HEADDER -->
+
+<section class=" header-section section bg-1 mb-5" style="height: 240px;">
+    <div class="container text-center aa">
+    </div>
+</section>
+
+<section class="section p-0">
+    <div class="container">
+        <div class="col-md-9 m-auto">
+
+            <div class="text-center mb-5">
+                <h3 class="mb-5 text-primary">SISTEMA DE ENERGIA SOLAR FOTOVOLTAICA</h3>
+                <p>
+                    A energia solar é proveniente da luz do sol e obtida por meio de módulos fotovoltaicos, que têm como
+                    função captar a energia luminosa, transformá-la em energia elétrica e atender as cargas elétricas na
+                    unidade consumidora.</p>
+            </div>
+
+            <div class="col-12">
+                <hr>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6 mb-5">
+                    <img src="assets/img/como/01.png" alt="1ª ETAPA: CAPTAÇÃO DE ENERGIA" class="img-fluid">
+                </div>
+                <div class="col-md-6 mb-5 align-middle">
+                    <h3 class="text-warning">1ª ETAPA: CAPTAÇÃO DE ENERGIA</h3>
+                    <p>A luz solar é captada pelos módulos e transformada em energia elétrica.</p>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6 mb-5 align-middle">
+                    <h3 class="text-warning">2ª ETAPA: TRANSFORMAR</h3>
+                    <p>A energia vai para o inversor fotovoltaico que converte-a de corrente contínua para corrente
+                        alternada, disponibilizando energia elétrica para o consumo.</p>
+                </div>
+                <div class="col-md-6 mb-5">
+                    <img src="assets/img/como/02.png" alt="2ª ETAPA: TRANSFORMAR" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-6 mb-5">
+                    <img src="assets/img/como/03.png" alt="3ª ETAPA: CONSUMIR" class="img-fluid">
+                </div>
+                <div class="col-md-6 mb-5 align-middle">
+                    <h3 class="text-warning">3ª ETAPA: CONSUMIR</h3>
+                    <p>A energia elétrica gerada pelo sistema fotovoltaico prioritariamente abastece as cargas elétricas
+                        da unidade consumidora (residência ou empresa) e, caso exista um excedente de geração de energia
+                        a mesma é direcionada para a rede pública de energia através do relógio medidor bidirecional,
+                        transformando os valores em créditos que podem ser usados como desconto em tarifas futuras, por
+                        até 5 anos.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include ("include/footer.php"); ?>
